fa78f8b9c879008b385c8225fc2790c6d252f078
JavaScript 2020/21 <javascript202021@Admins-iMac-28.local>
2020-12-18 20:46:37 +0200

    javascript curs 1

---
+++ hello.js
@@ -0,0 +1,15 @@
+var time = 3000
+time = time +1000
+var message = "Bine ai venit!"
+var isLate = false
+
+setTimeout(welcome, time)
+
+function welcome() {
+    alert(message)
+    alert("Wellcome!")
+}
+
+
+
+
