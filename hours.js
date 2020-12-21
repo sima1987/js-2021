46914f7177b030d0909e5567a067f639aa67191b
JavaScript 2020/21 <javascript202021@Admins-iMac-28.local>
2020-12-21 20:38:38 +0200

    js-2021

---
+++ hours.js
@@ -0,0 +1,13 @@
+var hour = 0
+
+while (hour < 24) {
+    document.writeln(hour)
+    document.writeln("<br>")
+    hour++
+    
+}
+for (var hour = 0; hour < 24; hour++) {
+    document.writeln(hour)
+    document.writeln("<br>")
+
+}
