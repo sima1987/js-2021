a90bb0ff27bb7a5459947ca5702c803c11c951fc
JavaScript 2020/21 <javascript202021@Admins-iMac-28.local>
2021-01-15 20:48:00 +0200

    array

---
+++ array/array/array.js
@@ -0,0 +1,13 @@
+let bubbles = [60, 59, 48, 71, 45, 56, 67, 65, 87, 32]
+
+let fruits = ["ananas", "mango", "oragnge", "tomato", "cucumber"] 
+
+bubbles.sort()
+max = bubbles.pop()
+
+fruits.sort()
+fruits.reverse()
+
+console.log(max)
+console.log(fruits)
+
