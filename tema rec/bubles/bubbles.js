74d7cbb902aeead3960017c5a02e9a802c940920
JavaScript 2020/21 <javascript202021@Admins-iMac-28.local>
2021-01-18 19:43:13 +0200

    bubble

--- tema rec/bubles/bubbles.js
+++ tema rec/bubles/bubbles.js
@@ -6,9 +6,19 @@ function newBubble() {
     let bubble = document.createElement("div")
     bubble.classList.add("bubble")
     let x = randomNumber(100)
-    let delay = randomNumber(3000)
-    bubble.style.left = x + "vw"
-    bubble.style.animationDelay = delay + "ms"
+    let delay = randomNumber(5000)
+    let red = randomNumber (255)
+    let green = randomNumber(255)
+    let blue = randomNumber(255)
+    let size = randomNumber(3) + 1
+    //bubble.style.left =  x + "vw"
+    bubble.style.left =  `${x}vw`
+    //bubble.style.animationDelay = delay + "ms"
+    bubble.style.animationDelay = `${delay}ms`
+    //bubble.style.borderColor = "rgb(" + red + "," + green + "," + blue + ")"
+    bubble.style.borderColor = `rgb(${red} , ${green},${blue})`
+    bubble.style.height = bubble.style.width = `${size}em` 
+    
     document.querySelector("body").appendChild(bubble)
 }
 
